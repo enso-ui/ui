1c7569432f3d05aa4068b08269c416fb976d9bc1
mishu <mishu@xtelecom.ro>
2019-06-23 09:46:35 +0300

    fixes #14, #21

--- src/modules/plugins/date-fns/unicode2php.js
+++ src/modules/plugins/date-fns/unicode2php.js
@@ -1,30 +1,37 @@
-const map = {
-    Y: 'yyyy', // 1999
-    y: 'yy', // 99
-    F: 'MMMM', // January
-    M: 'MMM', // Jan
-    m: 'MM', // 01
-    d: 'dd', // 19
-    j: 'd', //
-    l: 'EEEE', // Tuesday
-    D: 'EE', // Tue
-    a: 'a', // Am/PM
-    p: 'p', // Am/PM
-    H: 'HH', // 24-hour with leading zeros
-    G: 'H', // 24-hour without leading zeros
-    h: 'h', // 12-hour with leading zeros
-    g: 'K', // 12-hour without leading zero
-    i: 'mm', // Minutes with leading zeros
-    s: 'ss', // Seconds, with leading zeros
-    T: 'zz', // Timezone abbreviation
-    e: 'zzzz', // Timezone
-};
+const map = [
+    { replace: 'Y', with: 'yyyy' }, // 1999
+    { replace: 'y', with: 'yy' }, // 99
+    { replace: 'Y', with: 'y' }, // 1999
+    { replace: 'F', with: 'MMMM' }, // January
+    { replace: 'M', with: 'MMM' }, // Jan
+    { replace: 'm', with: 'MM' }, // 01
+    { replace: 'd', with: 'dd' }, // 19
+    { replace: 'j', with: 'd' }, //
+    { replace: 'l', with: 'EEEE' }, // Tuesday
+    { replace: 'D', with: 'EEE' }, // Tue
+    { replace: 'D', with: 'EE' }, // Tue
+    { replace: 'a', with: 'a' }, // Am/PM
+    { replace: 'p', with: 'p' }, // Am/PM
+    { replace: 'H', with: 'HH' }, // 24-hour with leading zeros
+    { replace: 'G', with: 'H' }, // 24-hour without leading zeros
+    { replace: 'h', with: 'h' }, // 12-hour with leading zeros
+    { replace: 'g', with: 'K' }, // 12-hour without leading zero
+    { replace: 'i', with: 'mm' }, // Minutes with leading zeros
+    { replace: 's', with: 'ss' }, // Seconds, with leading zeros
+    { replace: 'T', with: 'zz' }, // Timezone abbreviation
+    { replace: 'e', with: 'zzzz' }, // Timezone
+];
+
 
 const segments = formatStr => formatStr
-    .split(new RegExp([' ', '-', '/', ':', ','].join('|'), ''))
+    .split(new RegExp([' ', '-', '/', ':', ',', '[.]'].join('|'), ''))
     .sort((a, b) => b.length - a.length); // ASC -> a - b; DESC -> b - a
 
+const mapping = segment => map
+    .find(({ replace }) => replace === segment).with;
+
 const unicode2PHP = formatStr => segments(formatStr)
-    .reduce((string, segment) => string.replace(segment, map[segment] || ''), formatStr);
+    .reduce((string, segment) => string
+        .replace(segment, mapping(segment)) || '', formatStr);
 
 export default unicode2PHP;

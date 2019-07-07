3343fb66fc5642c9d658b1012d3403f51228357b
mishu <mishu@xtelecom.ro>
2019-07-07 12:33:24 +0300

    extracted avatar list in its own component (teams)

---
+++ src/bulma/pages/administration/teams/components/AvatarList.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="is-flex avatar-list">
+        <figure class="image is-32x32 has-margin-right-small has-margin-bottom-small"
+            v-for="user in users"
+            :key="user.id"
+            v-tooltip="user.name">
+            <img class="is-rounded"
+                :src="route('core.avatars.show', user.avatar.id)">
+        </figure>
+    </div>
+</template>
+
+<script>
+import { VTooltip } from 'v-tooltip';
+
+export default {
+    name: 'AvatarList',
+
+    directives: { tooltip: VTooltip },
+
+    props: {
+        users: {
+            type: Array,
+            required: true,
+        },
+    },
+};
+</script>
+
+<style lang="scss">
+    .avatar-list {
+        margin-left: 1em;
+        flex-flow: wrap;
+
+        figure {
+            transition: margin 0.2s;
+            -webkit-transition: margin 0.2s;
+            transition: transform .2s;
+            -webkit-transition: transform 0.2s;
+            margin-left: -1em;
+
+            &:hover {
+                margin-left: -0.5em;
+                margin-right: 1em;
+                transform: scale(1.3);
+                z-index: 2;
+            }
+
+            img {
+                border: 2px solid #f4f6fb;
+                border-radius: 50%;
+
+                &:hover {
+                    border: none;
+                }
+            }
+        }
+    }
+</style>
--- src/bulma/pages/administration/teams/components/Team.vue
+++ src/bulma/pages/administration/teams/components/Team.vue
@@ -60,19 +60,11 @@
             </div>
         </div>
         <div class="box-body has-padding-medium">
-            <div class="is-flex avatar-list">
-                <figure class="image is-32x32 has-margin-right-small has-margin-bottom-small"
-                    v-for="user in team.users"
-                    :key="user.id"
-                    v-tooltip="user.name">
-                    <img class="is-rounded"
-                        :src="avatar(user.avatar.id)">
-                </figure>
-                <span v-if="!team.edit && !loading && team.users.length === 0"
-                    class="has-text-muted is-italic has-margin-bottom-small">
-                    {{ i18n('No users yet') }}
-                </span>
-            </div>
+            <avatar-list :users="team.users"/>
+            <span v-if="!team.edit && !loading && team.users.length === 0"
+                class="has-text-muted is-italic has-margin-bottom-small">
+                {{ i18n('No users yet') }}
+            </span>
             <fade>
                 <div class="has-margin-bottom-large has-margin-top-large"
                     v-if="team.edit && team.name">
@@ -96,8 +88,8 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 import { focus } from '@enso-ui/directives';
 import { Fade } from '@enso-ui/transitions';
-import { VTooltip } from 'v-tooltip';
 import { EnsoSelect } from '@enso-ui/bulma';
+import AvatarList from './AvatarList.vue';
 
 library.add([faBan, faPencilAlt, faTrash, faCheck]);
 
@@ -106,9 +98,9 @@ export default {
 
     inject: ['errorHandler', 'i18n'],
 
-    directives: { tooltip: VTooltip, focus },
+    directives: { focus },
 
-    components: { Fade, EnsoSelect },
+    components: { Fade, EnsoSelect, AvatarList },
 
     props: {
         team: {
@@ -152,9 +144,6 @@ export default {
                     this.$emit('destroy');
                 }).catch(this.errorHandler);
         },
-        avatar(id) {
-            return route('core.avatars.show', id);
-        },
     },
 };
 </script>
@@ -162,7 +151,6 @@ export default {
 <style lang="scss">
     .team-name {
         width: 200px;
-
         border-top: unset;
         border-left: unset;
         border-right: unset;
@@ -171,35 +159,6 @@ export default {
 
         &:focus {
             box-shadow: unset;
-        }
-    }
-
-    .avatar-list {
-        margin-left: 1em;
-        flex-flow: wrap;
-
-        figure {
-            transition: margin 0.2s;
-            -webkit-transition: margin 0.2s;
-            transition: transform .2s;
-            -webkit-transition: transform 0.2s;
-            margin-left: -1em;
-
-            &:hover {
-                margin-left: -0.5em;
-                margin-right: 1em;
-                transform: scale(1.3);
-                z-index: 2;
-            }
-
-            img {
-                border: 2px solid #f4f6fb;
-                border-radius: 50%;
-
-                &:hover {
-                    border: none;
-                }
-            }
         }
     }
 </style>

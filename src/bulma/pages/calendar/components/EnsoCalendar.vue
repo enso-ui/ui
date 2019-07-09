df227e84307507890364f222f8392cdf63c1e4ca
Adrian Ocneanu <aocneanu@gmail.com>
2019-07-09 07:18:42 +0300

    Revert "fix arabic language"

--- src/bulma/pages/calendar/components/EnsoCalendar.vue
+++ src/bulma/pages/calendar/components/EnsoCalendar.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="calendar-wrapper box is-paddingless raises-on-hover">
         <vue-cal :time-from="7 * 60"
-            :locale="lang === 'ar' ? 'en' : lang"
+            :locale="lang"
             :events="events"
             show-all-day-events
             :disable-views="['years']"

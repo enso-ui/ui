7f1b4306e61e7635ad16a0cfbfc1bb599754cae3
mishu <mishu@xtelecom.ro>
2019-08-03 15:20:19 +0300

    fixes number format

--- src/modules/filters/numberFormat.js
+++ src/modules/filters/numberFormat.js
@@ -4,6 +4,7 @@ import store from '@root/store';
 Vue.filter('numberFormat', (value, decimals = 3) => new Intl.NumberFormat(
     store.state.preferences.global.lang, {
         style: 'decimal',
-        maximumSignificantDigits: decimals,
+        maximumFractionDigits: decimals,
+        minimumFractionDigits: decimals,
     },
 ).format(value));

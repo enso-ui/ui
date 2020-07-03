b51f244a902ffaad22c9c534851883b3317faa4f
Vlad Chiovianu <vlad.chiovianu@earthlink.ro>
2020-07-03 13:33:52 +0300

    removes how-to/activity-log/tutorials dependencies

--- src/bulma/routes.js
+++ src/bulma/routes.js
@@ -7,14 +7,11 @@ const routes = routeImporter(require.context('./routes', false, /.*\.js$/));
     .add(routeImporter(require.context('@enso-ui/companies/src/bulma/routes', false, /.*\.js$/)))
     .add(routeImporter(require.context('@enso-ui/data-import/src/bulma/routes', false, /.*\.js$/)))
     .add(routeImporter(require.context('@enso-ui/files/src/bulma/routes', false, /.*\.js$/)))
-    .add(routeImporter(require.context('@enso-ui/how-to/src/bulma/routes', false, /.*\.js$/)))
     .add(routeImporter(require.context('@enso-ui/localisation/src/bulma/routes', false, /.*\.js$/)))
     .add(routeImporter(require.context('@enso-ui/menus/src/bulma/routes', false, /.*\.js$/)))
     .add(routeImporter(require.context('@enso-ui/people/src/bulma/routes', false, /.*\.js$/)))
     .add(routeImporter(require.context('@enso-ui/permissions/src/bulma/routes', false, /.*\.js$/)))
     .add(routeImporter(require.context('@enso-ui/roles/src/bulma/routes', false, /.*\.js$/)))
     .add(routeImporter(require.context('@enso-ui/teams/src/bulma/routes', false, /.*\.js$/)))
-    .add(routeImporter(require.context('@enso-ui/tutorials/src/bulma/routes', false, /.*\.js$/)))
     .add(routeImporter(require.context('@enso-ui/logs/src/bulma/routes', false, /.*\.js$/)))
-    .add(routeImporter(require.context('@enso-ui/activity-log/src/bulma/routes', false, /.*\.js$/)));
 export default routes;

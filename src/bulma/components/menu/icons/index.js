5ac9c2326ebd529b9426e4c3391f68bcafc486d6
Mihai <mihai.ocneanu@gmail.com>
2020-11-06 12:57:23 +0200

    added missing icon

--- src/bulma/components/menu/icons/index.js
+++ src/bulma/components/menu/icons/index.js
@@ -2,10 +2,10 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 
 import {
     faTachometerAlt, faCogs, faUser, faUserTie, faUsers, faAddressBook,
-    faSlidersH, faObjectGroup, faExclamationTriangle, faAngleUp,
+    faSlidersH, faObjectGroup, faExclamationTriangle, faAngleUp, faTerminal,
 } from '@fortawesome/free-solid-svg-icons';
 
 library.add([
     faTachometerAlt, faCogs, faUser, faUserTie, faUsers, faAddressBook,
-    faSlidersH, faObjectGroup, faExclamationTriangle, faAngleUp,
+    faSlidersH, faObjectGroup, faExclamationTriangle, faAngleUp, faTerminal,
 ]);

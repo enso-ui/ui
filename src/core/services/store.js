b4438537076c00ce4feff02c3b46cd5c7ee43e93
mishu <mishu@xtelecom.ro>
2021-06-15 22:37:12 +0300

    small refactor;

--- src/core/services/store.js
+++ src/core/services/store.js
@@ -1,25 +1,23 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import Resources from './resources.js';
-import storeImporter from '../../modules/importers/storeImporter';
+import importer from '../../modules/importers/storeImporter';
 import {
     actions, getters, modules, mutations, state,
 } from '../../modules/store.js';
 
 const packageModules = () => {
-    const modules = storeImporter(Resources.store());
+    const modules = importer(Resources.store());
 
-    return Object.keys(modules)
-        .reduce((module, key) => {
-            module[key.split('/').pop()] = modules[key];
-
-            return module;
-        }, {});
+    return Object.keys(modules).reduce((module, key) => {
+        module[key.split('/').pop()] = modules[key];
+        return module;
+    }, {});
 };
 
 const store = {
     strict: true,
-    modules: { ...modules, ...packageModules(), ...storeImporter(Resources.localStore()) },
+    modules: { ...modules, ...packageModules(), ...importer(Resources.localStore()) },
     state,
     getters,
     mutations,

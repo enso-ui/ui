1b105f8cea19e5079ec89fbdffffc2d7af58f060
mishu <mishu@xtelecom.ro>
2021-06-15 22:30:30 +0300

    fixes circular reference

--- src/core/services/store.js
+++ src/core/services/store.js
@@ -1,7 +1,31 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import StoreBuilder from './storeBuilder';
+import Resources from './resources.js';
+import storeImporter from '../../modules/importers/storeImporter';
+import {
+    actions, getters, modules, mutations, state,
+} from '../../modules/store.js';
+
+const packageModules = () => {
+    const modules = storeImporter(Resources.store());
+
+    return Object.keys(modules)
+        .reduce((module, key) => {
+            module[key.split('/').pop()] = modules[key];
+
+            return module;
+        }, {});
+};
+
+const store = {
+    strict: true,
+    modules: { ...modules, ...packageModules(), ...storeImporter(Resources.localStore()) },
+    state,
+    getters,
+    mutations,
+    actions,
+};
 
 Vue.use(Vuex);
 
-export default new Vuex.Store(new StoreBuilder().handle());
+export default new Vuex.Store(store);

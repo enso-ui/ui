682dbd3a2f44b2302bb6b30cfe32a464ccfdec5b
Mihai <mihai.ocneanu@gmail.com>
2021-12-03 18:20:01 +0200

    wip

--- src/bulma/components/navbar/NavbarItem.vue
+++ src/bulma/components/navbar/NavbarItem.vue
@@ -51,6 +51,8 @@ export default {
         },
     },
 
+    emits: ['click'],
+
     data: () => ({
         dropdown: false,
         alwaysVisible: false,
--- src/core/components/navbar/Navbar.vue
+++ src/core/components/navbar/Navbar.vue
@@ -4,18 +4,24 @@ import eventBus from '../../../core/services/eventBus';
 
 export default {
     name: 'Navbar',
+
     inject: ['canAccess'],
+
+    emits: ['stop-impersonating'],
+
     computed: {
         ...mapState(['meta', 'impersonating']),
         ...mapState('layout', ['isMobile', 'isTouch', 'sidebar']),
         ...mapGetters('layout/navbar', ['items']),
     },
+
     methods: {
         ...mapMutations('layout/sidebar', { toggleSidebar: 'toggle' }),
         stopImpersonating() {
             eventBus.$emit('stop-impersonating');
         },
     },
+
     render() {
         return this.$slots.default({
             meta: this.meta,

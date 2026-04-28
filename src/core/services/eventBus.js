bf8e01766ee0362fe7a7ddb5ab11741520ce5883
mishu <mishu@xtelecom.ro>
2026-04-28 19:16:30 +0300

    Fix event bus ESM compatibility

--- src/core/services/eventBus.js
+++ src/core/services/eventBus.js
@@ -1,8 +1,50 @@
-import emitter from 'tiny-emitter/instance';
+const listeners = new Map();
 
-export default {
-  $on: (...args) => emitter.on(...args),
-  $once: (...args) => emitter.once(...args),
-  $off: (...args) => emitter.off(...args),
-  $emit: (...args) => emitter.emit(...args)
+const callbacksFor = event => listeners.get(event) ?? [];
+
+const eventBus = {
+    $on(event, callback) {
+        listeners.set(event, [...callbacksFor(event), callback]);
+
+        return eventBus;
+    },
+
+    $once(event, callback) {
+        const listener = (...args) => {
+            eventBus.$off(event, listener);
+            callback(...args);
+        };
+
+        listener.callback = callback;
+
+        return eventBus.$on(event, listener);
+    },
+
+    $off(event, callback) {
+        if (!callback) {
+            listeners.delete(event);
+
+            return eventBus;
+        }
+
+        const nextCallbacks = callbacksFor(event)
+            .filter(listener => listener !== callback && listener.callback !== callback);
+
+        if (nextCallbacks.length) {
+            listeners.set(event, nextCallbacks);
+        } else {
+            listeners.delete(event);
+        }
+
+        return eventBus;
+    },
+
+    $emit(event, ...args) {
+        callbacksFor(event).slice()
+            .forEach(callback => callback(...args));
+
+        return eventBus;
+    },
 };
+
+export default eventBus;
